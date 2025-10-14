7f1f1b968ff74c28e8501e5c9a9155501e70aa13
Monica R <138276052+monica-r56@users.noreply.github.com>
2025-10-14 22:45:39 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(() => ({ 
+const REPO_NAME = 'portfolio'; 
+
+export default defineConfig(() => ({
+  base: `/${REPO_NAME}/`,
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react()], 
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
